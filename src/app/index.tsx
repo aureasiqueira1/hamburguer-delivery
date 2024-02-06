735a49b0bd8d6f1eb0ae4f6aec5d61eced77d88c
aureasiqueira1 <aurea@gec.inatel.br>
2024-02-06 14:45:49 -0300

    configuração do tailwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello React</Text>
+    <View className="bg-slate-900 flex 1">
+      <Text className="text-white">s Rffeact</Text>
       <Text>Hello React</Text>
       <Text>Hello React</Text>
     </View>
