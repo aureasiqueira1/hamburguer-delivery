555f540087318a494b78755f414d04f11a72dcae
aureasiqueira1 <aurea@gec.inatel.br>
2024-02-06 14:22:28 -0300

    criação do projeto react native

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Hello React</Text>
+      <Text>Hello React</Text>
+      <Text>Hello React</Text>
+    </View>
+  );
+}
