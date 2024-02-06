624829866bb5a9a3d143ca83d3c31a6c275ea240
aureasiqueira1 <aurea@gec.inatel.br>
2024-02-06 15:52:48 -0300

    Aula 01

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,37 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, FlatList } from "react-native";
+
+import { CATEGORIES } from "@/utils/data/products";
+
+import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button";
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
-    <View className="bg-slate-900 flex 1">
-      <Text className="text-white">s Rffeact</Text>
-      <Text>Hello React</Text>
-      <Text>Hello React</Text>
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantityItems={3} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
